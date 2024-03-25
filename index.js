5c729c7aa5f2bf3d014bc96fff0a1b2e318362d8
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2024-03-25 22:40:40 +0000

    Added basic working endpoints

---
+++ index.js
@@ -0,0 +1,72 @@
+require('dotenv').config();
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize({
+	dialect: 'sqlite',
+	storage: 'database.sqlite',
+	logging: false
+})
+const UnapprovedMessage = sequelize.define('unapproved_message', {
+	text: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+},
+	{
+		timestamps: false
+	})
+const MessageApproved = sequelize.define('message', {
+	text: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+},
+	{
+		timestamps: false
+	})
+
+sequelize.sync()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.post("/addmsg", async (req, res) => {
+	const { text } = req.body
+	console.log(text)
+	const msg = await UnapprovedMessage.create({ text }).catch(err => {
+		console.log(err)
+		return res.json({ message: "Error" })
+	})
+	console.log(msg)
+	res.json({ message: "Message sent for approval", text })
+})
+app.get("/get_unapproved_msgs", async (req, res) => {
+	const messages = await UnapprovedMessage.findAll()
+	res.json(messages)
+})
+app.get("/get_approved_msgs", async (req, res) => {
+	const messages = await MessageApproved.findAll()
+	res.json(messages)
+})
+app.post("/approve", async (req, res) => {
+	const { id, passwd } = req.body
+	if (passwd !== process.env.SECRET_PASSWORD) {
+		return res.json({ message: "Invalid password" })
+	}
+	const message = await UnapprovedMessage.findOne({ where: { id } })
+	if (!message) {
+		return res.json({ message: "Message not found" })
+	}
+	try {
+		console.log(await UnapprovedMessage.destroy({ where: { id } }))
+		await MessageApproved.create({ text: message.text })
+		res.json({ message: "Message approved" })
+	} catch (err) {
+		console.log(err)
+		res.json({ message: `Error: ${err.message}` })
+	}
+})
+app.listen(3000, () => {
+	console.log("Server is running")
+})
