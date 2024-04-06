1dbcad2f0f5431c74a1be2076eddd0f00ae73223
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2024-04-06 03:17:04 +0000

    resolved problems with CORS

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ async function main() {
 
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({ extended: true }))
+	app.use((req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*")
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		next()
+	})
 
 	app.get("/", (req, res) => {
 		res.send("Hello")
